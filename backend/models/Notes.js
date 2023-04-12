03749c3182173fb59354056443a01a373e34d9c5
Dracorex-xD <DracorexLee@gmail.com>
2023-04-12 12:39:58 +1000

    Updated
    removed unnecessary test code

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 
 const noteSchema = mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-    },
+    
     title: {
         type: String,
         required: [true, `Please enter some text`],
