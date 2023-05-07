ca8d38c5c83f060833cb507bd064627f55a16979
Dracorex-xD <DracorexLee@gmail.com>
2023-05-07 15:14:49 +1000

    initial start to vue version of notes-app
    I really don't like React

--- frontend/src/components/Note.jsx
+++
@@ -1,9 +0,0 @@
-export const Note = (props) => {
-  return (
-    <div className="Note">
-      <h1 className="note-title">{props.title}</h1>
-      <p className="note-text">{props.text}</p>
-      <div className="timestamp">{props.time}</div>
-    </div>
-  );
-}
--- frontend/src/components/Searchbar.jsx
+++
@@ -1,8 +0,0 @@
-const Searchbar = () => {
-  return (
-    <div><input className="Searchbar"type="text" id="Search" placeholder="Searchbar"/></div>
-  )
-}
-
-
-export default Searchbar;
